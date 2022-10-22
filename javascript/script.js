6b5a048ab653b41fdb7d797a5ceaecdc3d8b50cf
= <christregear@gmail.com>
2022-10-22 02:03:50 +0100

    Search array

--- javascript/script.js
+++ javascript/script.js
@@ -2,15 +2,22 @@ let myShoppingList = ["fruit", "bread", "chicken", "beef", "cakes", "vegetables"
 
 let items = ''
 
-function createItemList(arr){
-for(let i=0; i < myShoppingList.length; i++){
-    items +=  `<li>${ arr[i]} </li>`
- }
- return items;
+// function createItemList(arr){
+// for(let i=0; i < myShoppingList.length; i++){
+//     items +=  `<li>${ arr[i]} </li>`
+//  }
+//  return items;
+// }
+
+
+let ask = prompt('Which item are you searching for?')
+
+if(myShoppingList.includes(ask)){
+    message = `Yes, we have <strong>${ask}</strong>`;
+} else {
+    message=`Sorry, we do not have <strong>${ask}</strong>`
 }
 
 document.querySelector('main').innerHTML = `
-    <ol>
-        ${createItemList(myShoppingList)}
-    </ol>
+    <p>${message}</p>
 `;
